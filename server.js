85553c8cf72a6ecfb539acb87e494c38ac04e419
yolixtly <yolixtly_13@hotmail.com>
2017-01-19 17:10:06 -0500

    created a simple web server using express

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-import https from 'https';
+import config from './config';
+import express from 'express';
 
-https.get('https://www.lynda.com', res => {
-  console.log('Response status code: ', res.statusCode);
-  res.on('data', chunk => {
-    console.log(chunk.toString());
-  });
+const server = express();
+
+server.get('/', (req,res) => {
+  res.send('Hello Express');
+});
+
+server.get('/about.html', (req,res) => {
+  res.send('the about page');
+});
+
+server.listen(config.port, ()=> {
+  console.info('Express listening on port', config.port);
 });
