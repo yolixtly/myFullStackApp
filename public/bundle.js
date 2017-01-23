7a017f96d70811f02d58a4c016b60a2a18786d12
yolixtly <yolixtly_13@hotmail.com>
2017-01-22 20:31:18 -0500

    Differences between Stateless and Stateful components React

--- public/bundle.js
+++ public/bundle.js
@@ -57,7 +57,7 @@
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
-	var _app = __webpack_require__(/*! ./components/app */ 179);
+	var _app = __webpack_require__(/*! ./components/app */ 178);
 	
 	var _app2 = _interopRequireDefault(_app);
 	
@@ -22075,8 +22075,7 @@
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! ./../../process/browser.js */ 3)))
 
 /***/ },
-/* 178 */,
-/* 179 */
+/* 178 */
 /*!*******************************!*\
   !*** ./src/components/app.js ***!
   \*******************************/
@@ -22088,29 +22087,69 @@
 	  value: true
 	});
 	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(/*! react */ 1);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var _header = __webpack_require__(/*! ./header */ 180);
+	var _header = __webpack_require__(/*! ./header */ 179);
 	
 	var _header2 = _interopRequireDefault(_header);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	var App = function App(props) {
-	  return _react2.default.createElement(
-	    'div',
-	    null,
-	    _react2.default.createElement(_header2.default, { message: 'Naming Contests' }),
-	    _react2.default.createElement(
-	      'div',
-	      { className: 'text-right' },
-	      'Made by ',
-	      props.name
-	    )
-	  );
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var App = function (_React$Component) {
+	  _inherits(App, _React$Component);
+	
+	  function App() {
+	    var _ref;
+	
+	    var _temp, _this, _ret;
+	
+	    _classCallCheck(this, App);
+	
+	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	      args[_key] = arguments[_key];
+	    }
+	
+	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = App.__proto__ || Object.getPrototypeOf(App)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+	      pageHeader: "Naming Contests"
+	    }, _temp), _possibleConstructorReturn(_this, _ret);
+	  }
+	
+	  _createClass(App, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(_header2.default, { message: this.state.pageHeader }),
+	        _react2.default.createElement(
+	          'div',
+	          null,
+	          this.state.test
+	        ),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'text-right' },
+	          'Made by ',
+	          this.props.name
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return App;
+	}(_react2.default.Component);
+	
+	;
 	
 	// defaultProps will be used to ensure that this.props.name will have a value if it was not specified by the parent component
 	App.defaultProps = {
@@ -22120,7 +22159,7 @@
 	exports.default = App;
 
 /***/ },
-/* 180 */
+/* 179 */
 /*!**********************************!*\
   !*** ./src/components/header.js ***!
   \**********************************/
